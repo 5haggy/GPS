2ad5e2d78e21f201eea76067d419a16ed5b3c243
5haggy <adaxks@gmail.com>
2018-11-22 10:02:13 +0100

    Update Fmin.m

--- Fmin.m
+++ Fmin.m
@@ -1,13 +1,16 @@
 %FUNKCJA fmin.m
-%   działanie: tworzy funkcję do minimalizacji
+%   dziaĹ‚anie: tworzy funkcjÄ™ do minimalizacji
 %   argumenty: x y z , dane satelit (xsat,ysat,zsat,dsat)
-%   zwraca:    funkcję do minimalizacji
+%   zwraca:    funkcjÄ™ do minimalizacji
 
 function [wsp Jac]= Fmin(xsat,ysat,zsat,dsat,J,x0,y0,z0)
     syms x y z;
    % i= 1:5;
     wsp = (dsat.^2 - ( (x0 - xsat).^2 + (y0 - ysat).^2 + (z0 - zsat).^2 ) );
     %if nargout > 1
-    Jac = double(subs(J,{x,y,z},{xsat,ysat,zsat})); % tutaj  do poprawy bo robi za duzo kolumn
-    %end
+    for n = 1:5
+    Jac(n,1) = double(subs(J(n,1),{x},{xsat(n)}));
+    Jac(n,2) = double(subs(J(n,2),{y},{ysat(n)}));
+    Jac(n,3) = double(subs(J(n,3),{z},{zsat(n)}));
+    end
 end
